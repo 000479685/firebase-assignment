b894c0e97c35392cda0cc5c1fb57cd45fbba7e99
Aiden Wu <wuazhihong@gmail.com>
2024-11-15 21:28:00 -0800

    For the sake of the deploy, this wil just be unsafe with no env configuration

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,13 +9,20 @@ import { getFirestore } from "@firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  apiKey: "AIzaSyBdeV7S0wCV1UKgZ-7e0A8b462pOtdgeYA",
+
+  authDomain: "blog-app-6d6f5.firebaseapp.com",
+
+  projectId: "blog-app-6d6f5",
+
+  storageBucket: "blog-app-6d6f5.firebasestorage.app",
+
+  messagingSenderId: "631768240373",
+
+  appId: "1:631768240373:web:ae7ba7a4378440a32db5bc",
+
+  measurementId: "G-KNHKJJPGER"
+
 };
 
 // Initialize Firebase
